409cbe7ed51086547d533a42b8c8ae4720f9da4b
InaSLew <ina.shenlewander@gmail.com>
2021-12-08 02:14:35 +0100

    Set spike to inactive when out of camera view

--- Assets/Scripts/Spikes/TriangleSpike.cs
+++ Assets/Scripts/Spikes/TriangleSpike.cs
@@ -1,5 +1,12 @@
+using System;
 using UnityEngine;
 
 public class TriangleSpike : MonoBehaviour
 {
+
+    private void OnBecameInvisible()
+    {
+        gameObject.SetActive(false);
+        // probably reset position back to spawn as well
+    }
 }
