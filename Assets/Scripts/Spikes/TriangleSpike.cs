1d264ee52ee9005a1d2de8f996f1df9ff228f844
InaSLew <ina.shenlewander@gmail.com>
2021-12-09 12:34:04 +0100

    Added trigger to triangle spike to detect overcome triangle spikes

--- Assets/Scripts/Spikes/TriangleSpike.cs
+++ Assets/Scripts/Spikes/TriangleSpike.cs
@@ -1,12 +1,18 @@
-using System;
 using UnityEngine;
 
 public class TriangleSpike : MonoBehaviour
 {
     [SerializeField] private Vector3Value spawnPosition;
+    [SerializeField] private GameEvent IsSpikeOvercome;
+    
     private void OnBecameInvisible()
     {
         gameObject.SetActive(false);
         transform.localPosition = spawnPosition.Vector3;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) IsSpikeOvercome.Raise();
     }
 }
