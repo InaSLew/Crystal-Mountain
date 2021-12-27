cdd8936eb746a5c9bdc4439d7a44d4dd31709620
InaSLew <ina.shenlewander@gmail.com>
2021-12-27 22:14:43 +0100

    Script CountFailAttempt attached to UI component CountFailAttempt

--- Assets/Scripts/UI/CountFailAttempt.cs
+++ Assets/Scripts/UI/CountFailAttempt.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CountFailedAttempt : MonoBehaviour
+public class CountFailAttempt : MonoBehaviour
 {
     private int totalAttempts = 0;
     private Text failAttemptDisplay;
