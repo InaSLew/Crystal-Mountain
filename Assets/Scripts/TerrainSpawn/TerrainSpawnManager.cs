dffd2f6d02dce73f68f5e0470b677a552f8f9570
InaSLew <ina.shenlewander@gmail.com>
2021-12-29 11:22:55 +0100

    Removed unused script and added summary comments to script in TerrainSpawn folder

--- Assets/Scripts/TerrainSpawn/TerrainSpawnManager.cs
+++ Assets/Scripts/TerrainSpawn/TerrainSpawnManager.cs
@@ -1,13 +1,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Spawns the crystal ground and keeps it moving through queuing and dequeuing from the spike queue.
+/// </summary>
 public class TerrainSpawnManager : MonoBehaviour
 {
+    [Header("Crystal prefab and how many to spawn")]
     [SerializeField] private GameObject crystalPrefab;
-    [SerializeField] private Vector3 startPosition;
     [SerializeField] private IntValue numberOfCrystal;
+    
+    [Header("Endless-scrolling-related")]
+    [SerializeField] private Vector3 startPosition;
     [SerializeField] private FloatValue playerDistanceTravelled;
     [SerializeField] private float recycleOffset;
+    
     private Vector3 worldSpawnPosition;
     private Queue<GameObject> crystalQueue;
     private Vector3 nextPosition;
